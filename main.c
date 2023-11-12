7eb70a5d11f59598a2a979739560e83d42856c9c
Nermirc <141329241+Nermirc@users.noreply.github.com>
2023-11-12 23:20:07 +0300

    Average of The Numbers
    This program calculates average of the numbers

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+    float ortalamabul(int *dizim,int boyut)
+    {
+        int i;
+        int toplam=0;
+        for(i=0;i<boyut;i++)
+        {
+            toplam+=dizim[i];
+        }
+        return (float)toplam/boyut;
+    }
+int main()
+{
+    //ortalama(int *dizim,int boyut)*
+    int n;
+    int i;
+    float ort;
+    printf("Kac adet eleman girilecek ?\n");
+    scanf("%d",&n);
+    int dizi[n];
+    for(i=0;i<n;i++)
+    {
+        printf("%d. sayiyi giriniz:\n",i+1);
+        scanf("%d",&dizi[i]);
+    }
+    ort=ortalamabul(dizi,n);
+    printf("Girdigimiz sayilarin ortalamasi %.3f",ort);
+
+
+
+}
